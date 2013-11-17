6cbf9dd0db2de5ac2888e97a92450fb39c0b4a7b
vagrant <vagrant@ironhack.(none)>
2013-11-17 16:34:05 +0000

    adding comments

--- lib/connection_diagnostic.rb
+++ lib/connection_diagnostic.rb
@@ -6,5 +6,6 @@ class ConnectionDiagnostic
   # tries to connect the client 3 times
   # if unsuccessful raise Error
   # if successful sends 'AT#UD' to client (diagnostic message)
-  #   and receives from the client
+  #   and receives from the client the diagnostic info
+  #   this info should be accesible on the attribute info
 end
