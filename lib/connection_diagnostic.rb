4488d8a1e0e1cc64274c225f6b08d205553196bf
vagrant <vagrant@ironhack.(none)>
2013-11-17 15:15:21 +0000

    better namming

---
+++ lib/connection_diagnostic.rb
@@ -0,0 +1,10 @@
+require 'client'
+
+class ConnectionDiagnostic
+  # perform_diagnostic
+  # disconnects the client
+  # tries to connect the client 3 times
+  # if unsuccessful raise Error
+  # if successful sends 'AT#UD' to client (diagnostic message)
+  #   and receives from the client
+end
