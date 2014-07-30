b2df3e36e2bc75f4bc86d7f8561b4aee59f71460
xsimov <xsimov@gmail.com>
2014-07-30 13:10:28 +0200

    'Tries to connect three times' feature done

--- lib/connection_diagnostic.rb
+++ lib/connection_diagnostic.rb
@@ -1,11 +1,28 @@
 require 'client'
 
 class ConnectionDiagnostic
+
+  def initialize(client)
+    @client = client
+  end
+
+  def perform_diagnostic
+    @client.disconnect
+    status = client_connection
+  end
+
+  def client_connection(times_tried = 0)
+    this_iteration = @client.connect("any non-empty str")
+    return (client_connection(times_tried + 1)) unless (this_iteration || times_tried == 2)
+    this_iteration
+  end
+
   # perform_diagnostic
   # disconnects the client
   # tries to connect the client 3 times
-  # if unsuccessful raise Error
-  # if successful sends 'AT#UD' to client (diagnostic message)
-  #   and receives from the client the diagnostic info
-  #   this info should be accesible on the attribute info
+  #   if unsuccessful raise Error
+  #   if successful sends 'AT#UD' to client (diagnostic message)
+  #     and receives from the client the diagnostic info
+  #     this info should be accesible on the attribute info
+
 end
--- spec/connection_diagnostic_spec.rb
+++ spec/connection_diagnostic_spec.rb
@@ -2,5 +2,37 @@ require 'spec_helper'
 
 describe ConnectionDiagnostic do
 
+  context 'the perform diagnostic method' do
+
+    before(:each) do
+      @client = double("Client")
+      allow(@client).to receive(:disconnect)
+      allow(@client).to receive(:connect).with("any non-empty str").and_return(true)
+      @connection = ConnectionDiagnostic.new(@client)
+    end
+
+    it "disconnects the client" do
+      expect(@client).to receive(:disconnect)
+      @connection.perform_diagnostic
+    end
+
+    it "tries to connect the client" do
+      expect(@client).to receive(:connect).once
+      @connection.perform_diagnostic
+    end
+
+    it "retries connection if unsuccessful" do
+      allow(@client).to receive(:connect).with("any non-empty str").and_return(false, true)
+      expect(@client).to receive(:connect).twice
+      @connection.perform_diagnostic
+    end
+
+    it "retries connection maximum 3 times" do
+      allow(@client).to receive(:connect).with("any non-empty str").and_return(false, false, false)
+      expect(@client).to receive(:connect).exactly(3).times
+      @connection.perform_diagnostic
+    end
+
+  end
 
 end
